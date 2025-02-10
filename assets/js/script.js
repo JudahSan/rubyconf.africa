b754a0a414ca1c7e60474123c5451312765dc282
judahsan <baraka.judah@gmail.com>
2025-02-10 10:07:56 +0300

    Update FAQ, fix format issues and remove bloat

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,422 +1,223 @@
-(function($) {
-    "use strict";
+(function ($) {
+  "use strict";
 
-    // Windows load
+  // Handle preloader
+  function handlePreloader() {
+    // Check for the old preloader (preloader)
+    if ($(".preloader").length) {
+      // Show the old preloader (fade-in)
+      $(".preloader").fadeIn(200);
 
-    $(window).on("load", function() {
+      // Hide the old preloader after a delay and fade it out
+      setTimeout(function () {
+        $(".preloader").fadeOut(200); // Fade out the old preloader
+      }, 2000); // Adjust delay (e.g., 2 seconds)
+    }
+    // Check for the new preloader (preloader-new)
+    else if ($(".preloader-new").length) {
+      // Show the new preloader (fade-in)
+      $(".preloader-new").fadeIn(200);
 
-        // Site loader 
+      // Hide the new preloader after the page has fully loaded
+      $(window).on("load", function () {
+        $(".preloader-new").fadeOut(200); // Fade out the new preloader
+      });
+    }
+  }
 
-        $(".loader-inner").fadeOut();
-        $(".loader").delay(200).fadeOut("slow");
+  // Run the handlePreloader function when the page is ready
+  $(document).ready(function () {
+    handlePreloader();
+  });
 
+  // Windows load
+
+  $(window).on("load", function () {
+    // Site loader
+
+    $(".loader-inner").fadeOut();
+    $(".loader").delay(200).fadeOut("slow");
+  });
+
+  // Currency
+  $("#currency").change(function () {
+    var selectedCurrency = $(this).val();
+
+    $(".amount").each(function () {
+      var newPrice = $(this).data(selectedCurrency);
+      $(this).text(newPrice);
     });
 
-    // Currency
-    $("#currency").change(function() {
-        var selectedCurrency = $(this).val();
+    $(".cur").text(selectedCurrency === "kes" ? "KSh" : "$");
+  });
 
-        $(".amount").each(function() {
-            var newPrice = $(this).data(selectedCurrency);
-            $(this).text(newPrice);
-        });
+  // Scroll to
 
-        $(".cur").text(selectedCurrency === "kes" ? "KSh" : "$");
-    })
+  $("a.scroll").smoothScroll({
+    speed: 800,
+    offset: -50,
+  });
 
+  // Slider
 
-    // Scroll to
+  $(".slider").flexslider({
+    animation: "fade",
+    slideshow: true,
+    directionNav: true,
+    controlNav: false,
+    pauseOnAction: false,
+    animationSpeed: 500,
+  });
 
-    $('a.scroll').smoothScroll({
-        speed: 800,
-        offset: -50
-    });
+  $(".review-slider").flexslider({
+    animation: "fade",
+    slideshow: true,
+    directionNav: false,
+    controlNav: true,
+    pauseOnAction: false,
+    animationSpeed: 1000,
+  });
 
+  // Mobile menu
 
-    // Slider
+  var mobileBtn = $(".mobile-but");
+  var nav = $(".main-nav ul");
+  var navHeight = nav.height();
 
-    $('.slider').flexslider({
-        animation: "fade",
-        slideshow: true,
-        directionNav: true,
-        controlNav: false,
-        pauseOnAction: false,
-        animationSpeed: 500
-    });
+  $(mobileBtn).on("click", function () {
+    $(".toggle-mobile-but").toggleClass("active");
+    nav.slideToggle();
+    $(".main-nav li a").addClass("mobile");
+    return false;
+  });
 
+  $(window).resize(function () {
+    var w = $(window).width();
+    if (w > 320 && nav.is(":hidden")) {
+      nav.removeAttr("style");
+      $(".main-nav li a").removeClass("mobile");
+    }
+  });
 
-    $('.review-slider').flexslider({
-        animation: "fade",
-        slideshow: true,
-        directionNav: false,
-        controlNav: true,
-        pauseOnAction: false,
-        animationSpeed: 1000
-    });
+  $(".main-nav li a").on("click", function () {
+    if ($(this).hasClass("mobile")) {
+      nav.slideToggle();
+      $(".toggle-mobile-but").toggleClass("active");
+    }
+  });
 
+  // Append images as css background
 
+  $(".background-img").each(function () {
+    var path = $(this).children("img").attr("src");
+    $(this)
+      .css("background-image", 'url("' + path + '")')
+      .css("background-position", "initial");
+  });
 
-    // Mobile menu
+  // Count down setup
 
-    var mobileBtn = $('.mobile-but');
-    var nav = $('.main-nav ul');
-    var navHeight = nav.height();
+  $(".countdown").countdown("2025/7/18", function (event) {
+    $(this).html(event.strftime("%D days %H:%M:%S"));
+  });
 
-    $(mobileBtn).on("click", function() {
-        $(".toggle-mobile-but").toggleClass("active");
-        nav.slideToggle();
-        $('.main-nav li a').addClass('mobile');
-        return false;
+  // Tabbed content
 
+  $(".block-tabs li").on("click", function () {
+    if (!$(this).hasClass("active")) {
+      var tabNum = $(this).index();
+      var nthChild = tabNum + 1;
+      $(".block-tabs li.active").removeClass("active");
+      $(this).addClass("active");
+      $(".block-tab li.active").removeClass("active");
+      $(".block-tab li:nth-child(" + nthChild + ")").addClass("active");
+    }
+  });
 
-    });
+  // Zoom	effect
 
-    $(window).resize(function() {
-        var w = $(window).width();
-        if (w > 320 && nav.is(':hidden')) {
-            nav.removeAttr('style');
-            $('.main-nav li a').removeClass('mobile');
-        }
+  $(".block-gallery li").on("mouseenter", function () {
+    $(this).closest(".gallery").find(".block-gallery li").removeClass("active");
+    $(this).addClass("active");
+  });
 
-    });
+  $(".block-ticket").on("mouseenter", function () {
+    $(this).closest(".tickets").find(".block-ticket").removeClass("active");
+    $(this).addClass("active");
+  });
 
-    $('.main-nav li a').on("click", function() {
-        if ($(this).hasClass('mobile')) {
-            nav.slideToggle();
-            $(".toggle-mobile-but").toggleClass("active");
-        }
+  // Images zoom
 
-    });
+  $(".venobox").venobox({
+    titleattr: "data-title",
+    numeratio: true,
+  });
 
+  // Instagram feed setup
 
+  var instaFeed = new Instafeed({
+    get: "user",
+    userId: "305801553",
+    accessToken: "305801553.1677ed0.3d872300c10c4ff687868875ee8abc5d",
+    limit: 6,
+    template:
+      '<li class="col-sm-4"><a href="{{link}}"><img src="{{image}}"/></a></li>',
+  });
+  instaFeed.run();
 
-    // Append images as css background
+  // Form validation
 
-    $('.background-img').each(function() {
-        var path = $(this).children('img').attr('src');
-        $(this).css('background-image', 'url("' + path + '")').css('background-position', 'initial');
-    });
-
-
-
-
-    // Count down setup
-
-    $('.countdown').countdown('2025/7/18', function(event) {
-        $(this).html(event.strftime('%D days %H:%M:%S'));
-    });
-
-
-
-
-    // Tabbed content 
-
-    $(".block-tabs li").on("click", function() {
-        if (!$(this).hasClass("active")) {
-            var tabNum = $(this).index();
-            var nthChild = tabNum + 1;
-            $(".block-tabs li.active").removeClass("active");
-            $(this).addClass("active");
-            $(".block-tab li.active").removeClass("active");
-            $(".block-tab li:nth-child(" + nthChild + ")").addClass("active");
-        }
-    });
-
-
-    // Zoom	effect
-
-    $('.block-gallery li').on("mouseenter", function() {
-        $(this).closest('.gallery').find('.block-gallery li').removeClass('active');
-        $(this).addClass('active');
-    });
-
-
-
-    $('.block-ticket').on("mouseenter", function() {
-        $(this).closest('.tickets').find('.block-ticket').removeClass('active');
-        $(this).addClass('active');
-    });
-
-
-
-
-    // Images zoom 
-
-    $('.venobox').venobox({
-        titleattr: 'data-title',
-        numeratio: true
-    });
-
-
-
-    // Instagram feed setup
-
-    var instaFeed = new Instafeed({
-        get: 'user',
-        userId: '305801553',
-        accessToken: '305801553.1677ed0.3d872300c10c4ff687868875ee8abc5d',
-        limit: 6,
-        template: '<li class="col-sm-4"><a href="{{link}}"><img src="{{image}}"/></a></li>'
-    });
-    instaFeed.run();
-
-
-    // Form validation 
-
-    // var resgistryForm = $('.registry-form');
-    // resgistryForm.validate({
-    //     validClass: 'valid',
-    //     errorClass: 'error',
-    //     errorPlacement: function(error, element) {
-    //         return true;
-    //     },
-    //     onfocusout: function(element, event) {
-    //         $(element).valid();
-    //     },
-    //     rules: {
-    //         email: {
-    //             required: true,
-    //             email: true
-    //         }
-    //     },
-    //
-    //     rules: {
-    //         name: {
-    //             required: true,
-    //             minlength: 3
-    //         }
-    //     }
-    //
-    //
-    // });
-
-
-
+  // var resgistryForm = $('.registry-form');
+  // resgistryForm.validate({
+  //     validClass: 'valid',
+  //     errorClass: 'error',
+  //     errorPlacement: function(error, element) {
+  //         return true;
+  //     },
+  //     onfocusout: function(element, event) {
+  //         $(element).valid();
+  //     },
+  //     rules: {
+  //         email: {
+  //             required: true,
+  //             email: true
+  //         }
+  //     },
+  //
+  //     rules: {
+  //         name: {
+  //             required: true,
+  //             minlength: 3
+  //         }
+  //     }
+  //
+  //
+  // });
 })(jQuery);
-
-
-// Map setup 
-
-// function initializeMap() {
-//
-//
-//
-//     var styles = [{
-//                 "elementType": "geometry",
-//                 "stylers": [{
-//                     "color": "#212121"
-//                 }]
-//             },
-//             {
-//                 "elementType": "labels.icon",
-//                 "stylers": [{
-//                     "visibility": "off"
-//                 }]
-//             },
-//             {
-//                 "elementType": "labels.text.fill",
-//                 "stylers": [{
-//                     "color": "#757575"
-//                 }]
-//             },
-//             {
-//                 "elementType": "labels.text.stroke",
-//                 "stylers": [{
-//                     "color": "#212121"
-//                 }]
-//             },
-//             {
-//                 "featureType": "administrative",
-//                 "elementType": "geometry",
-//                 "stylers": [{
-//                     "color": "#757575"
-//                 }]
-//             },
-//             {
-//                 "featureType": "administrative.country",
-//                 "elementType": "labels.text.fill",
-//                 "stylers": [{
-//                     "color": "#9e9e9e"
-//                 }]
-//             },
-//             {
-//                 "featureType": "administrative.land_parcel",
-//                 "stylers": [{
-//                     "visibility": "off"
-//                 }]
-//             },
-//             {
-//                 "featureType": "administrative.locality",
-//                 "elementType": "labels.text.fill",
-//                 "stylers": [{
-//                     "color": "#bdbdbd"
-//                 }]
-//             },
-//             {
-//                 "featureType": "poi",
-//                 "elementType": "labels.text.fill",
-//                 "stylers": [{
-//                     "color": "#757575"
-//                 }]
-//             },
-//             {
-//                 "featureType": "poi.park",
-//                 "elementType": "geometry",
-//                 "stylers": [{
-//                     "color": "#181818"
-//                 }]
-//             },
-//             {
-//                 "featureType": "poi.park",
-//                 "elementType": "labels.text.fill",
-//                 "stylers": [{
-//                     "color": "#616161"
-//                 }]
-//             },
-//             {
-//                 "featureType": "poi.park",
-//                 "elementType": "labels.text.stroke",
-//                 "stylers": [{
-//                     "color": "#1b1b1b"
-//                 }]
-//             },
-//             {
-//                 "featureType": "road",
-//                 "elementType": "geometry.fill",
-//                 "stylers": [{
-//                     "color": "#2c2c2c"
-//                 }]
-//             },
-//             {
-//                 "featureType": "road",
-//                 "elementType": "labels.text.fill",
-//                 "stylers": [{
-//                     "color": "#8a8a8a"
-//                 }]
-//             },
-//             {
-//                 "featureType": "road.arterial",
-//                 "elementType": "geometry",
-//                 "stylers": [{
-//                     "color": "#373737"
-//                 }]
-//             },
-//             {
-//                 "featureType": "road.highway",
-//                 "elementType": "geometry",
-//                 "stylers": [{
-//                     "color": "#3c3c3c"
-//                 }]
-//             },
-//             {
-//                 "featureType": "road.highway.controlled_access",
-//                 "elementType": "geometry",
-//                 "stylers": [{
-//                     "color": "#4e4e4e"
-//                 }]
-//             },
-//             {
-//                 "featureType": "road.local",
-//                 "elementType": "labels.text.fill",
-//                 "stylers": [{
-//                     "color": "#616161"
-//                 }]
-//             },
-//             {
-//                 "featureType": "transit",
-//                 "elementType": "labels.text.fill",
-//                 "stylers": [{
-//                     "color": "#757575"
-//                 }]
-//             },
-//             {
-//                 "featureType": "water",
-//                 "elementType": "geometry",
-//                 "stylers": [{
-//                     "color": "#000000"
-//                 }]
-//             },
-//             {
-//                 "featureType": "water",
-//                 "elementType": "labels.text.fill",
-//                 "stylers": [{
-//                     "color": "#3d3d3d"
-//                 }]
-//             }
-//         ],
-//
-//         lat = -1.2534411,
-//         lng = 36.8568778,
-//
-//
-//
-//
-//         customMap = new google.maps.StyledMapType(styles, {
-//             name: 'Styled Map'
-//         }),
-//
-//
-//         mapOptions = {
-//             zoom: 14,
-//             scrollwheel: false,
-//             disableDefaultUI: true,
-//             draggable: true,
-//             center: new google.maps.LatLng(lat, lng),
-//             mapTypeControlOptions: {
-//                 mapTypeIds: [google.maps.MapTypeId.ROADMAP]
-//             }
-//         },
-//         map = new google.maps.Map(document.getElementById('map'), mapOptions),
-//         myLatlng = new google.maps.LatLng(lat, lng),
-//
-//
-//
-//
-//         marker = new google.maps.Marker({
-//             position: myLatlng,
-//             map: map,
-//             icon: {
-//                 url: 'img/marker.png',
-//                 scaledSize: new google.maps.Size(80, 80)
-//             }
-//         });
-//
-//
-//
-//
-//     map.mapTypes.set('map_style', customMap);
-//     map.setMapTypeId('map_style');
-//
-//
-//
-//     var transitLayer = new google.maps.TransitLayer();
-//     transitLayer.setMap(map);
-//
-//
-//
-// }
 
 // Replace Google Maps whti Leaflet
 function initializeMap() {
-    var lat = -1.2534411,
-        lng = 36.8568778;
+  var lat = -1.2534411,
+    lng = 36.8568778;
 
-    // Initialize Leaflet map
-    var map = L.map('map').setView([lat, lng], 14);
+  // Initialize Leaflet map
+  var map = L.map("map").setView([lat, lng], 14);
 
-    // Add OpenStreetMap tiles
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        attribution: '&copy; OpenStreetMap contributors'
-    }).addTo(map);
+  // Add OpenStreetMap tiles
+  L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    attribution: "&copy; OpenStreetMap contributors",
+  }).addTo(map);
 
-    // Custom map marker
-    var markerIcon = L.icon({
-        iconUrl: 'img/marker.png',
-        iconSize: [80,80]
-    });
+  // Custom map marker
+  var markerIcon = L.icon({
+    iconUrl: "img/marker.png",
+    iconSize: [80, 80],
+  });
 
-    // Add marker to map
-    L.marker([lat, lng], { icon: markerIcon }).addTo(map).bindPopup("Your Location").openPopup();
-
+  // Add marker to map
+  L.marker([lat, lng], { icon: markerIcon })
+    .addTo(map)
+    .bindPopup("Your Location")
+    .openPopup();
 }
-document.addEventListener('DOMContentLoaded', initializeMap);
+document.addEventListener("DOMContentLoaded", initializeMap);
